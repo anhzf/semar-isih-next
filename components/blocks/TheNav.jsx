26b3b88b098479022a9899ea07f8a398bd606d87
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-03-03 00:02:00 +0700

    feat(page): :sparkles: add dropdown to about nav item

--- components/blocks/TheNav.jsx
+++ components/blocks/TheNav.jsx
@@ -1,13 +1,41 @@
+import { useMemo, useRef } from 'react';
 import { Button } from 'primereact/button';
+import { Menu } from 'primereact/menu';
 
-export default function TheNav ({ reveal }) {
+export default function TheNav({ reveal }) {
   const bgColor = reveal ? 'bg-white' : 'bg-transparent';
+  const menuRef = useRef(null);
+  const aboutDropdownItems = useMemo(() => [
+    {
+      label: 'About Semar-ISIH',
+      command(e) {
+        menuRef.current.hide(e);
+        window.location.assign('/#about');
+      },
+    },
+    {
+      label: 'About UNS',
+      url: '/about-uns',
+    },
+  ]);
 
   return (
     <nav className={`sticky hidden top-0 z-50 w-full px-16 py-4 ${bgColor} sm:flex flex-row flex-nowrap justify-end gap-x-8 shadow-md`}>
-      <a href="#about">
-        <Button label="About" className="p-button-text p-button-rounded" />
-      </a>
+      <div>
+        <Menu
+          model={aboutDropdownItems}
+          id="popup_menu"
+          ref={menuRef}
+          popup
+        />
+        <Button
+          label="About"
+          className="p-button-text p-button-rounded"
+          onClick={e => menuRef.current.toggle(e)}
+          aria-controls="popup_menu"
+          aria-haspopup
+        />
+      </div>
 
       <a href="#guideline">
         <Button label="Guideline" className="p-button-text p-button-rounded" />
