6b5569d0fe4d56a31a4f8c0ae9a841d65ffabb87
Anhzf <36941633+Epiclygend@users.noreply.github.com>
2021-03-05 18:11:06 +0700

    feat: :sparkles: create Menu Component (still basic functionallity)

---
+++ components/Menu.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useCallback, useImperativeHandle } from 'react';
+
+const Menu = React.forwardRef(function ({
+  stateHandler: [visible, setVisible] = useState(false),
+  children,
+}, ref) {
+  const [[x, y], setPos] = useState([0, 0])
+  const open = useCallback(
+    e => {
+      setVisible(!visible);
+      const { bottom, left } = e.target.getBoundingClientRect();
+
+      setPos([bottom, left]);
+    },
+    [visible],
+  );
+
+  useImperativeHandle(
+    ref,
+    () => ({ open }),
+    [visible],
+  );
+
+  return (
+    visible && (
+      <div
+        className="fixed top-0 left-0 rounded-lg border shadow-lg"
+        style={{
+          top: `${x}px`,
+          left: `${y}px`
+        }}
+      >
+        <ul className="relative flex flex-col">
+          {children}
+        </ul>
+      </div>)
+  );
+});
+
+const Item = function ({
+  children,
+}) {
+  return (
+    <li className="cursor-pointer px-5 py-4 bg-white first:rounded-t-lg last:rounded-b-lg hover:bg-gray-100">{children}</li>
+  );
+};
+
+export default Object.assign(Menu, {
+  Item,
+});
--- components/blocks/TheNav.jsx
+++ components/blocks/TheNav.jsx
@@ -1,44 +1,59 @@
 import { useMemo, useRef } from 'react';
+import Link from 'next/link';
 import { useRouter } from 'next/router'
 import { Button } from 'primereact/button';
-import { Menu } from 'primereact/menu';
+import Menu from 'components/Menu';
+// import { Menu } from 'primereact/menu';
 
 export default function TheNav({ reveal }) {
   const router = useRouter();
   const bgColor = reveal ? 'bg-white' : 'bg-transparent';
   const menuRef = useRef(null);
-  const aboutDropdownItems = useMemo(() => [
-    {
-      label: 'About the event',
-      command(e) {
-        menuRef.current.hide(e);
-        router.push('/#about');
-      },
-    },
-    {
-      label: 'About UNS',
-      command(e) {
-        router.push('/about-uns');
-      }
-    },
-  ]);
+  // const aboutDropdownItems = useMemo(() => [
+  //   {
+  //     label: 'About the event',
+  //     command(e) {
+  //       menuRef.current.hide(e);
+  //       router.push('/#about');
+  //     },
+  //   },
+  //   {
+  //     label: 'About UNS',
+  //     command(e) {
+  //       router.push('/about-uns');
+  //     }
+  //   },
+  // ]);
 
   return (
     <nav className={`sticky hidden top-0 z-50 w-full px-16 py-4 ${bgColor} sm:flex flex-row flex-nowrap justify-end gap-x-8 shadow-md`}>
-      <div>
-        <Menu
+      <div className="relative">
+        {/* <Menu
           model={aboutDropdownItems}
           id="popup_menu"
           ref={menuRef}
           popup
-        />
+        /> */}
         <Button
           label="About"
           className="p-button-text p-button-rounded"
-          onClick={e => menuRef.current.toggle(e)}
+          onClick={e => menuRef.current.open(e)}
           aria-controls="popup_menu"
           aria-haspopup
         />
+        <Menu ref={menuRef}>
+          <Menu.Item>
+            <Link href="/#about">
+              <a>About the Events</a>
+            </Link>
+          </Menu.Item>
+
+          <Menu.Item>
+            <Link href="/about-uns">
+              <a>About UNS</a>
+            </Link>
+          </Menu.Item>
+        </Menu>
       </div>
 
       <a href="#terms">
